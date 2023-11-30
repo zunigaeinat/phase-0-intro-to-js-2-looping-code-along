edfae56fedd5d60453cfded92c1143d1055738b9
Einat Zuniga <zunigaeinat@gmail.com>
2023-11-29 23:20:58 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const name = ["Becca", "Max", "Gab"];
+const eventName = "birthday"
+function writeCards(names, eventName) {
+  let greetings = [];
+
+  for (let i = 0; i < names.length; i++) {
+    let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+    greetings.push(message);
+  }
+
+  return greetings;
+}
+
+function countDown(num) {
+ while (num >= 0) {
+  console.log(num);
+  num--;
+ }
+}
+countDown(10)
